384c2feb5a4d86e9d19c5cd5c7cf415094c04fe8
dsgsjk <dsgsjk@connect.hku.hk>
2024-03-07 21:22:41 +0800

    Add filter command

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { workspace, ExtensionContext } from 'vscode';
+import { exec } from 'child_process';
 
 import {
 	LanguageClient,
@@ -10,6 +11,9 @@ import {
 let client: LanguageClient;
 
 export async function activate(context: ExtensionContext) {
+	const folder = vscode.window.activeTextEditor ? vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor.document.uri) : undefined;
+	const path = folder ? folder.uri.fsPath : undefined;
+
 	let helloWorldCmd = vscode.commands.registerCommand(`ants.helloWorld`, () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
@@ -20,9 +24,9 @@ export async function activate(context: ExtensionContext) {
 	});
 
 	let initCmd = vscode.commands.registerCommand(`ants.init`, () => {
-		let terminal = vscode.window.createTerminal("Terminal");
-		terminal.sendText("ants init");
+		exec(`ants init`, { cwd: path }, (error, stdout, stderr) => {});
 	});
+
 
 	let newCmd = vscode.commands.registerCommand(`ants.new`, async () => {
 		let title = await vscode.window.showInputBox({placeHolder: "Title"});
@@ -34,53 +38,243 @@ export async function activate(context: ExtensionContext) {
 			return;
 		}
 		
+
 		let dir = await vscode.window.showInputBox({placeHolder: "Directory"});
 		if(dir === undefined) {
 			return;
 		}
+
 		if(dir === '') {
 			vscode.window.showErrorMessage('empty directory');
 			return;
+
 		}
 		
-		let terminal = vscode.window.createTerminal("Terminal");
-		terminal.sendText(`ants new -t ${title} ${dir}`);
-		console.log(`ants new -t ${title} ${dir}`);
+		exec(`ants new -t ${title} ${dir}`, { cwd: path }, (error, stdout, stderr) => {});
+
 	});
+	
+	let filterCmd = vscode.commands.registerCommand(`ants.filter`, async () => {
+		const panel = vscode.window.createWebviewPanel(
+			'Filter',
+			'Filter',
+			vscode.ViewColumn.Beside,
+			{
+				enableScripts : true
+			}
+		);
+
+		
+		panel.webview.html = `
+			<!DOCTYPE html>
+
+			<html>
+			<head>
+				<meta charset="UTF-8">
+				<title>File Search</title>
+			</head>
+
+			<body>
+	 			<input type="text" id="author" placeholder="Author">
+				<input type="text" id="date" placeholder="Date">
+				<input type="text" id="description" placeholder="Description">
+				<input type="text" id="tag" placeholder="Tag">
+
+				<input type="text" id="content" placeholder="Content">
+				<ul id="searchResults"></ul>
+
+				<script>
+
+					const vscode = acquireVsCodeApi(); // Get the VSCode API object
+
+
+					const author = document.getElementById('author');
+					author.addEventListener('input', (event) => {
+						const query = event.target.value;
+						vscode.postMessage({ type: 'author', query });
+					});
+
+					
+					const date = document.getElementById('date');
+					date.addEventListener('input', (event) => {
+						const query = event.target.value;
+						vscode.postMessage({ type: 'date', query });
+					});
+					
+					const description = document.getElementById('description');
+					description.addEventListener('input', (event) => {
+						const query = event.target.value;
+						vscode.postMessage({ type: 'description', query });
+					});
+					
+					const tag = document.getElementById('tag');
+					tag.addEventListener('input', (event) => {
+						const query = event.target.value;
+						vscode.postMessage({ type: 'tag', query });
+
+					});
+					
+					const content = document.getElementById('content');
+					content.addEventListener('input', (event) => {
+						const query = event.target.value;
+						vscode.postMessage({ type: 'content', query });
+					});
+		 
+					window.addEventListener('message', (event) => {
+						const message = event.data;
+						if (message.type === 'searchResults') {
+							const searchResults = document.getElementById('searchResults');
+							searchResults.innerHTML = ''; // Clear previous results
+							message.results.forEach((result) => {
+								const li = document.createElement('li');
+								const a = document.createElement('a');
+
+								a.href = '#';
+								a.textContent = result;
+								a.addEventListener('click', (event) => {
+									event.preventDefault();
+									vscode.postMessage({ type: 'openFile', path: result });
+								});
+
+								li.appendChild(a);
+								searchResults.appendChild(li);
+							});
+						}
+
+					});
+				</script>
+			</body>
+
+			</html>
+		`;
+		
+		let terminal = vscode.window.createTerminal("Terminal");
+		let author = '', date = '', description = '', tag = '', content = '';
+		
+		panel.webview.onDidReceiveMessage((message) => {
+			if (message.type === 'openFile') {
+				const filePath = path + '/' + message.path;
+				vscode.workspace.openTextDocument(filePath).then((doc) => {
+					vscode.window.showTextDocument(doc);
+				});
+			} else {
+				const query = message.query;
+				
+				if (message.type === 'author') {
+					author = query;
+
+				} else if (message.type == 'date') {
+					date = query;
+				} else if (message.type == 'description') {
+					description = query;
+				} else if (message.type == 'tag') {
+					tag = query;
+				} else if (message.type == 'content') {
+
+					content = query;
+				}
+				let s = '';
+				if(author != '') {
+					if(s == '') {
+						s = `author:${author}`;
+					} else {
+
+						s += `&&author:${author}`;
+					}
+				}
+
+				if(date != '') {
+					if(s == '') {
+						s = `date:${date}`;
+
+					} else {
+						s += `&&date:${date}`;
+					}
+				}
+
+				if(description != '') {
+					if(s == '') {
+						s = `description:${description}`;
+
+					} else {
+						s += `&&description:${description}`;
+					}
+
+				}
+				if(tag != '') {
+					if(s == '') {
+
+						s = `tag:${tag}`;
+
+					} else {
+						s += `&&tag:${tag}`;
+					}
+
+				}
+				if(content != '') {
+					if(s == '') {
+						s = `content:${content}`;
+					} else {
+						s += `&&content:${content}`;
+					}
+				}
+				console.log(s);
+				exec(`ants filter ${s}`, { cwd: path }, (error, stdout, stderr) => {
+					let fileResults = stdout.split('\n');
+					fileResults.pop();
+					panel.webview.postMessage({ type: 'searchResults', results: fileResults });
+				});
+			}
+		}, undefined, context.subscriptions);
+		
+	});
+
 
 	context.subscriptions.push(
 		helloWorldCmd,
+
 		restartCmd,
 		initCmd,
-		newCmd
+		newCmd,
+		filterCmd
+
 	);
 
 	startClient(context)
+
 }
 
 export async function deactivate() {
+
 	await stopClient()
 }
 
 function startClient(context: ExtensionContext) {
+
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
+
 		run: { command: "ants-ls", args: [] },
 		debug: { command: "ants-ls", args: [] }
 	};
+
 
 	let clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'markdown' }],
 	};
 
+
 	client = new LanguageClient("ants", "ants", serverOptions, clientOptions);
+
 
 	context.subscriptions.push(client.start());
 }
 
 async function stopClient() {
+
 	if (!client) return;
 
 	await client.stop();
+
 }
